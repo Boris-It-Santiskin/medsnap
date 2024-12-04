69717fb49b025eb0672e19daa41ecf349d8d3d23
spawnkid19xx <borisisac@gmail.com>
2024-12-04 16:50:03 +0000

    [UPDATE] Model Customer structure

--- snap/settings.py
+++ snap/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import snap.secret_files
+from traceback import print_tb
+from secret_files.psql_Boris_data import psql_db, psql_password, psql_username, SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l-&tox7b&t%(3&$vhu#c341-0$%3kc_t#nndm(&(lalpd!)d2e'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my APPS
+    'customer.apps.CustomerConfig'
 ]
 
 MIDDLEWARE = [
@@ -73,20 +77,22 @@ WSGI_APPLICATION = 'snap.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+'''
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+'''
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': snap.secret_files.psql_Boris_data.psql_db,        # Database name
-        'USER': snap.secret_files.psql_Boris_data.psql_username,    # Database user
-        'PASSWORD': snap.secret_files.psql_Boris_data.psql_password,     # Database password
+        'NAME': psql_db,        # Database name
+        'USER': psql_username,    # Database user
+        'PASSWORD': psql_password,     # Database password
         'HOST': '127.0.0.1',        # Set to the database server's IP or hostname
         'PORT': '5432',             # Default PostgreSQL port
     }
