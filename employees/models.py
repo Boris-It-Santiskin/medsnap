ab196869462ad1f211bec88c3bdf01174eccb266
spawnkid19xx <borisisac@gmail.com>
2025-01-14 12:52:50 +0000

    [UPDATE] Added fields in employee moduls. has been added a gender for employees and speciality to dr module.

--- employees/models.py
+++ employees/models.py
@@ -14,6 +14,20 @@ class AbstractClinicalEmployee(AbstractUser):
         verbose_name = "Clinical Employee"
         verbose_name_plural = "Clinical Employees"
 
+
+    class EmployeeType(models.TextChoices):
+        select_employee_type = 'Click to select', 'Click to select'
+        dr = 'DR', 'DR'
+        reception = 'RECEPTION', 'RECEPTION'
+
+
+    class GenderClinicalEmployee(models.TextChoices):
+        select_gender = 'Click to select', 'Click to select'
+        male = 'Male', 'Male'
+        female = 'Female', 'Female'
+
+
+
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='clinicalemployee_set',  # Custom related name
@@ -26,16 +40,12 @@ class AbstractClinicalEmployee(AbstractUser):
         blank=True
     )
 
-    class EmployeeType(models.TextChoices):
-        dr = 'DR', 'DR'
-        reception = 'RECEPTION', 'RECEPTION'
 
     username = models.CharField(
         unique=False,
         blank=True,
         null=True
     )
-
 
     contact_number = models.CharField()
 
@@ -45,10 +55,17 @@ class AbstractClinicalEmployee(AbstractUser):
         help_text=_("Designates whether the user can log into this admin site."),
     )
 
-    employee_type = models.CharField(choices=EmployeeType, default='DR')
+    employee_type = models.CharField(
+        max_length=50,
+        choices=EmployeeType.choices,
+        default='Click to select'
+    )
 
-
-
+    gender_employee = models.CharField(
+        max_length=50,
+        choices=GenderClinicalEmployee.choices,
+        default='Click to select'
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.employee_type}"
@@ -58,6 +75,16 @@ class DRClinicalEmployee(AbstractClinicalEmployee):
         verbose_name = "DR"
         verbose_name_plural = "DR's"
 
+    class DrSpecialityType(models.TextChoices):
+        select_speciality = 'Click to select', 'Click to select'
+        s1 = 'SPECIALITY1', 'SPECIALITY1'
+        s2 = 'SPECIALITY2', 'SPECIALITY2'
+        s3 = 'SPECIALITY3', 'SPECIALITY3'
+        s4 = 'SPECIALITY4', 'SPECIALITY4'
+        s5 = 'SPECIALITY5', 'SPECIALITY5'
+        s6 = 'SPECIALITY6', 'SPECIALITY6'
+
+
     medical_order_ID = models.CharField(
         _("Medical Order ID"),
         unique=True,
@@ -65,6 +92,11 @@ class DRClinicalEmployee(AbstractClinicalEmployee):
         help_text=_("A unique identifier for the medical order assigned to this user. This ID is used to track the user's medical orders.")
     )
 
+    speciality_type = models.CharField(
+        max_length=50,
+        choices=DrSpecialityType.choices,
+        default='Click to select'
+    )
 
     def __str__(self):
         return f"Dr {self.first_name} {self.last_name} {self.medical_order_ID}"
